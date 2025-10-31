0e871d6af0928b43b3ff2c6ebe52175314313dd1
Fernando.Collantes <fernandocollantes.24@campuscamara.es>
2025-10-31 12:15:20 +0100

    cambio end point

--- server.js
+++ server.js
@@ -2,30 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 // Importar rutas
-app.use('/api/productos', require('./routes/productosRoutes',));
-app.use('/api/categorias', require('./routes/categoriasRoutes'));
-app.use('/api/carritos', require('./routes/carritosRoutes'));
-app.use('/api/proveedores', require('./routes/proveedoresRoutes'));
-app.use('/api/clientes', require('./routes/clientesRoutes'));
-app.use('/api/pedidos', require('./routes/pedidosRoutes'));
-
-/*
-app.use('/api/productos', require('./services/productosService',));
-app.use('/api/categorias', require('./services/categoriasService'));
-app.use('/api/carritos', require('./services/carritosService'));
-app.use('/api/proveedores', require('./services/proveedoresService'));
-app.use('/api/clientes', require('./services/clientesService'));
-app.use('/api/pedidos', require('./services/pedidosService'));
-
-
-app.use('/api/productos', require('./controllers/productosController',));
-app.use('/api/categorias', require('./controllers/categoriasController'));
-app.use('/api/carritos', require('./controllers/carritosController'));
-app.use('/api/proveedores', require('./controllers/proveedoresController'));
-app.use('/api/clientes', require('./controllers/clientesController'));
-app.use('/api/pedidos', require('./controllers/pedidosController'));
-*/
-
+app.use('/productos', require('./routes/productosRoutes',));
+app.use('/categorias', require('./routes/categoriasRoutes'));
+app.use('/carritos', require('./routes/carritosRoutes'));
+app.use('/proveedores', require('./routes/proveedoresRoutes'));
+app.use('/clientes', require('./routes/clientesRoutes'));
+app.use('/pedidos', require('./routes/pedidosRoutes'));
 
 
 

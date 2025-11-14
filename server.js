88812238aed942fd3a5c0071aec4e60b931345ae
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2025-11-14 13:22:40 +0100

    middleware

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+// Middleware para registrar todas las peticiones
+app.use((req, res, next) => {
+console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+next(); // continúa con la siguiente función
+});
+
+// Middleware para habilitar JSON
+app.use(express.json());
+
+// Middleware para habilitar formularios (body en formato URL-encoded)
+app.use(express.urlencoded({ extended: true }));
+
 // Importar rutas
 app.use('/productos', require('./routes/productosRoutes',));
 app.use('/categorias', require('./routes/categoriasRoutes'));
@@ -8,7 +21,6 @@ app.use('/carritos', require('./routes/carritosRoutes'));
 app.use('/proveedores', require('./routes/proveedoresRoutes'));
 app.use('/clientes', require('./routes/clientesRoutes'));
 app.use('/pedidos', require('./routes/pedidosRoutes'));
-
 
 
 
